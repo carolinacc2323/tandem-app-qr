7251081cca3ca5402dea0bb5f9a5be1fc6a8d515
naomitandem <carolinacc.tandem@gmail.com>
2024-09-10 13:14:05 +0200

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,37 @@
-import * as React from "react"
+import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./Footer"
 import NavbarTandem from "./NavbarTandem"
-
 const Layout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+  const stylemain={
+  minHeight:'80vh',
+  }
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.style.backgroundColor = '#000';
+      document.body.style.color = '#fff';
+    } else {
+      document.body.style.backgroundColor = '#fff';
+      document.body.style.color = '#000';
+    }
+  };
   return (
     <>
-      <NavbarTandem/>
-      <main>
-        {children}
+      <NavbarTandem darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <main style={stylemain}>
+      {children}
       </main>
-      <Footer/>
+      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
     </>
   )
 }
-
 export default Layout
+
+
+
+
+
+
+
